336e7b8e252cf5a9fc5bf03c9cdae4ad9238da42
christopher boucher <chrismichb@yahoo.com>
2021-05-07 02:50:31 -0400

    added images to monsters.js

--- monsters.js
+++ monsters.js
@@ -1,6 +1,7 @@
 const monsters = [{
   id: 1,
   name: 'Aknosom',
+  image: 'https://static.wikia.nocookie.net/monsterhunter/images/c/cf/MHRise-Aknosom_Icon.png/revision/latest/scale-to-width-down/80?cb=20210323165043',
   classification: 'Bird Wyvern',
   elements: 'Fire',
   habitat: 'Shrine Ruins, Frost Islands, Red Stronghold',
@@ -8,6 +9,7 @@ const monsters = [{
 }, {
   id: 2,
   name: 'Almudron',
+  image: 'https://static.wikia.nocookie.net/monsterhunter/images/9/9e/MHRise-Almudron_Icon.png/revision/latest/scale-to-width-down/80?cb=20210323165149',
   classification: 'Leviathan',
   elements: 'Water',
   habitat: 'Shrine Ruins, Sandy Plains, Flooded Forest, Red Stronghold',
@@ -15,6 +17,7 @@ const monsters = [{
 }, {
   id: 3,
   name: 'Anjanath',
+  image: 'https://static.wikia.nocookie.net/monsterhunter/images/7/76/MHRise-Anjanath_Icon.png/revision/latest/scale-to-width-down/80?cb=20210323171406',
   classification: 'Brute',
   elements: 'Fire',
   habitat: 'Shrine Ruins, Sandy Plains, Flooded Forest, Lava Caverns, Red Stronghold',
@@ -22,6 +25,7 @@ const monsters = [{
 }, {
   id: 4,
   name: 'Azuros',
+  image: 'https://static.wikia.nocookie.net/monsterhunter/images/9/95/MHRise-Arzuros_Icon.png/revision/latest/scale-to-width-down/80?cb=20210110013503',
   classification: 'Fanged Beast',
   elements: 'None',
   habitat: 'Shrine Ruins, Flooded Forest, Red Stronghold',
@@ -29,6 +33,7 @@ const monsters = [{
 }, {
   id: 5,
   name: 'Barioth',
+  image: 'https://static.wikia.nocookie.net/monsterhunter/images/f/ff/MHRise-Barioth_Icon.png/revision/latest/scale-to-width-down/80?cb=20210323165348',
   classification: 'Flying Wyvern',
   elements: 'Ice',
   habitat: 'Frost Islands',
