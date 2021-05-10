b8b76fd43e976bc52b45dbcb4d022d18f2319857
Hollis <hollisjamison@gmail.com>
2021-05-10 18:24:34 -0400

    Temporary working version of classification.pug

--- controller/monsters.js
+++ controller/monsters.js
@@ -1,54 +1,71 @@
-const monsters = require('../monsters')
+const monsters = require("../monsters");
 
 const getAllMonsters = (request, response) => {
-  return response.send(monsters)
-}
+  return response.send(monsters);
+};
 
 const getMonsterById = (request, response) => {
-  const { id } = request.params
+  const { id } = request.params;
 
-  const matchingMonsters = monsters.find((monster) => monster.id === parseInt(id))
+  const matchingMonsters = monsters.find(
+    (monster) => monster.id === parseInt(id)
+  );
 
   return matchingMonsters
     ? response.send(matchingMonsters)
-    : response.sendStatus(404)
-}
+    : response.sendStatus(404);
+};
 
 const monsterData = (request, response) => {
-  const { monsterData } = request.params
+  const { monsterData } = request.params;
 
-  const findMonster = monsters.filter((monster) => monster.classification.toLowerCase().includes(monsterData) ||
-    monster.elements.toString().toLowerCase().includes(monsterData))
+  const findMonster = monsters.filter(
+    (monster) =>
+      monster.classificationSlug.toLowerCase().includes(monsterData) ||
+      monster.elements.toString().toLowerCase().includes(monsterData)
+  );
 
-  return response.send(findMonster)
-}
+  // return response.send(findMonster)
+  console.log(findMonster)
+  return response.render('classification', { findMonster });
+};
 
 const getNextId = () => {
   const lastId = monsters.reduce((acc, monster) => {
-    return monster.id > acc ? monster.id : acc
-  }, 0)
+    return monster.id > acc ? monster.id : acc;
+  }, 0);
 
-  return lastId + 1
-}
+  return lastId + 1;
+};
 
 const saveNewMonster = (request, response) => {
-  const {
-    name, classification, elements, habitat, generation
-  } = request.body
+  const { name, classification, elements, habitat, generation } = request.body;
 
   if (!name || !classification || !elements || !habitat || !generation) {
     return response
       .status(400)
-      .send('The following fields are required: name, classification, elements, habitat, generation')
+      .send(
+        "The following fields are required: name, classification, elements, habitat, generation"
+      );
   }
 
   const newMonster = {
-    name, classification, elements, habitat, generation, id: getNextId()
-  }
+    name,
+    classification,
+    elements,
+    habitat,
+    generation,
+    id: getNextId(),
+  };
 
-  monsters.push(newMonster)
+  monsters.push(newMonster);
 
-  return response.status(201).send(newMonster)
-}
+  return response.status(201).send(newMonster);
+};
 
-module.exports = { getAllMonsters, getMonsterById, monsterData, saveNewMonster }
+module.exports = {
+  getAllMonsters,
+  getMonsterById,
+  monsterData,
+  saveNewMonster,
+};
--- monsters.js
+++ monsters.js
@@ -3,41 +3,44 @@ const monsters = [{
   name: 'Aknosom',
   image: 'https://static.wikia.nocookie.net/monsterhunter/images/c/cf/MHRise-Aknosom_Icon.png/revision/latest/scale-to-width-down/80?cb=20210323165043',
   classification: 'Bird Wyvern',
+  classificationSlug: 'bird-wyvern',
   elements: 'Fire',
   habitat: 'Shrine Ruins, Frost Islands, Red Stronghold',
   generation: 'Fifth'
-}, {
-  id: 2,
-  name: 'Almudron',
-  image: 'https://static.wikia.nocookie.net/monsterhunter/images/9/9e/MHRise-Almudron_Icon.png/revision/latest/scale-to-width-down/80?cb=20210323165149',
-  classification: 'Leviathan',
-  elements: 'Water',
-  habitat: 'Shrine Ruins, Sandy Plains, Flooded Forest, Red Stronghold',
-  generation: 'Fifth'
-}, {
-  id: 3,
-  name: 'Anjanath',
-  image: 'https://static.wikia.nocookie.net/monsterhunter/images/7/76/MHRise-Anjanath_Icon.png/revision/latest/scale-to-width-down/80?cb=20210323171406',
-  classification: 'Brute',
-  elements: 'Fire',
-  habitat: 'Shrine Ruins, Sandy Plains, Flooded Forest, Lava Caverns, Red Stronghold',
-  generation: 'Fifth'
-}, {
-  id: 4,
-  name: 'Azuros',
-  image: 'https://static.wikia.nocookie.net/monsterhunter/images/9/95/MHRise-Arzuros_Icon.png/revision/latest/scale-to-width-down/80?cb=20210110013503',
-  classification: 'Fanged Beast',
-  elements: 'None',
-  habitat: 'Shrine Ruins, Flooded Forest, Red Stronghold',
-  generation: 'Third'
-}, {
-  id: 5,
-  name: 'Barioth',
-  image: 'https://static.wikia.nocookie.net/monsterhunter/images/f/ff/MHRise-Barioth_Icon.png/revision/latest/scale-to-width-down/80?cb=20210323165348',
-  classification: 'Flying Wyvern',
-  elements: 'Ice',
-  habitat: 'Frost Islands',
-  generation: 'Third'
-}]
+}
+// }, {
+//   id: 2,
+//   name: 'Almudron',
+//   image: 'https://static.wikia.nocookie.net/monsterhunter/images/9/9e/MHRise-Almudron_Icon.png/revision/latest/scale-to-width-down/80?cb=20210323165149',
+//   classification: 'Leviathan',
+//   elements: 'Water',
+//   habitat: 'Shrine Ruins, Sandy Plains, Flooded Forest, Red Stronghold',
+//   generation: 'Fifth'
+// }, {
+//   id: 3,
+//   name: 'Anjanath',
+//   image: 'https://static.wikia.nocookie.net/monsterhunter/images/7/76/MHRise-Anjanath_Icon.png/revision/latest/scale-to-width-down/80?cb=20210323171406',
+//   classification: 'Brute',
+//   elements: 'Fire',
+//   habitat: 'Shrine Ruins, Sandy Plains, Flooded Forest, Lava Caverns, Red Stronghold',
+//   generation: 'Fifth'
+// }, {
+//   id: 4,
+//   name: 'Azuros',
+//   image: 'https://static.wikia.nocookie.net/monsterhunter/images/9/95/MHRise-Arzuros_Icon.png/revision/latest/scale-to-width-down/80?cb=20210110013503',
+//   classification: 'Fanged Beast',
+//   elements: 'None',
+//   habitat: 'Shrine Ruins, Flooded Forest, Red Stronghold',
+//   generation: 'Third'
+// }, {
+//   id: 5,
+//   name: 'Barioth',
+//   image: 'https://static.wikia.nocookie.net/monsterhunter/images/f/ff/MHRise-Barioth_Icon.png/revision/latest/scale-to-width-down/80?cb=20210323165348',
+//   classification: 'Flying Wyvern',
+//   elements: 'Ice',
+//   habitat: 'Frost Islands',
+//   generation: 'Third'
+// }
+]
 
 module.exports = monsters
